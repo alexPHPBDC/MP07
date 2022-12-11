0c8261f8e7d521b239dc47225a0b6f90b6e3248c
acalvo701 <93265280+acalvo701@users.noreply.github.com>
2022-12-11 17:04:44 +0100

    database connection

--- UF3/A1/Persistencia-i-CRUD/6-Concurs-de-gossos-amb-POO-i-PDO/classes/Database.php
+++ UF3/A1/Persistencia-i-CRUD/6-Concurs-de-gossos-amb-POO-i-PDO/classes/Database.php
@@ -3,9 +3,9 @@ class Database
 {
     private $host = "127.0.0.1";
     private $dbname = "concursgossos";
-    private $user = "root";
+    private $user = "alex";
     private $password = "patata";
-    private $port = "3308";
+    private $port = "3306";
 
     // Hold the class instance.
     private static $instance = null;
